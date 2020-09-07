302cdbe0d5f7e52bbaa19d18859f1fc00f8dd614
cristianCeamatu <cristian.ceamatu@gmail.com>
2020-09-07 15:58:16 +0300

    Add Authentication functionality Associated tweets with users Links for logged in/guests users

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_07_093939) do
+ActiveRecord::Schema.define(version: 2020_09_07_102416) do
 
   create_table "tweeets", force: :cascade do |t|
     t.text "tweeet"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_tweeets_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -26,8 +28,12 @@ ActiveRecord::Schema.define(version: 2020_09_07_093939) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "tweeets", "users"
 end
